1f0c1a13fa9719c529d8aa802915287d61f4d04c
Ivan Yiga <ivanatresyn@gmail.com>
2023-05-13 17:49:03 +0300

    Displayed the logged in user name

--- lib/common/SideMenu.dart
+++ lib/common/SideMenu.dart
@@ -1,13 +1,38 @@
+import 'package:dayalog/authentications/Login.dart';
 import 'package:dayalog/controllers/mainController.dart';
 import 'package:dayalog/styles/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert' as convert;
 
 import '../styles/colors.dart';
-class SideMenu extends StatelessWidget {
-  SideMenu({Key? key}) : super(key: key);
+class SideMenu extends StatefulWidget {
+  const SideMenu({Key? key}) : super(key: key);
+
+  @override
+  State<SideMenu> createState() => _SideMenuState();
+}
+
+class _SideMenuState extends State<SideMenu> {
 
   final mainController _mainController = Get.find();
+  var _userName = "";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getUser();
+  }
+
+  _getUser() async{
+    var userData = convert.jsonDecode(_mainController.userString.value);
+    // print(userData);
+    setState(() {
+      _userName = userData["userDetails"]["business_name"];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,37 +42,49 @@ class SideMenu extends StatelessWidget {
       children: <Widget>[
         DrawerHeader(
           child: SafeArea(
-            child: Container(
-              width: double.infinity,
-              height: 50,
-              decoration: BoxDecoration(
-                color: _mainController.themeChangeProvider.darkTheme?Colors.grey[900]:Colors.grey[100],
-                borderRadius: BorderRadius.circular(16)
-              ),
-              child: Center(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                        Icons.sunny
-                    ),
-                    Switch(
-                      // This bool value toggles the switch.
-                      value: _mainController.themeChangeProvider.darkTheme,
-                      activeColor: Colors.green,
-                      onChanged: (bool value) {
-                        // This is called when the user toggles the switch.
-                        // setState(() {
-                          _mainController.themeChangeProvider.darkTheme = value;
-                        // });
-                      },
-                    ),
-                    Icon(
-                      Icons.dark_mode
-                    )
-                  ],
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Hi $_userName",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold
+                  ),
                 ),
-              ),
+                SizedBox(height: 20,),
+                Container(
+                  width: double.infinity,
+                  // height: 50,
+                  decoration: BoxDecoration(
+                      color: _mainController.themeChangeProvider.darkTheme?Colors.grey[900]:Colors.grey[100],
+                      borderRadius: BorderRadius.circular(16)
+                  ),
+                  child: Center(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                            Icons.sunny
+                        ),
+                        Switch(
+                          // This bool value toggles the switch.
+                          value: _mainController.themeChangeProvider.darkTheme,
+                          activeColor: Colors.green,
+                          onChanged: (bool value) {
+                            // This is called when the user toggles the switch.
+                            // setState(() {
+                            _mainController.themeChangeProvider.darkTheme = value;
+                            // });
+                          },
+                        ),
+                        Icon(
+                            Icons.dark_mode
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           /*decoration: BoxDecoration(
@@ -57,6 +94,7 @@ class SideMenu extends StatelessWidget {
         SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
             children: [
               /*Container(
                 padding: EdgeInsets.only(left: 10),
@@ -75,17 +113,18 @@ class SideMenu extends StatelessWidget {
                     Icon(Icons.account_balance_wallet_rounded,
                       color: Colors.grey[600],),
                     SizedBox(width: 10,),
-                    Text('View Wallet',
+                    Text('Login Page',
                       style: TextStyle(
                           color: Colors.grey[600],
                           fontWeight: FontWeight.normal,
-                        fontSize: 16
+                          fontSize: 16
                       ),)
                   ],
                 ),
                 onTap: () async{
                   if(MediaQuery.of(context).size.width < 600){
                     Navigator.pop(context);
+                    Get.to(Login());
                   }
                 },
               ),
