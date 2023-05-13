7a23aee8610b656c7886793b39c277039a3eda89
Ivan Yiga <ivanatresyn@gmail.com>
2023-05-13 17:49:40 +0300

    added login controllers

--- lib/controllers/mainController.dart
+++ lib/controllers/mainController.dart
@@ -1,4 +1,5 @@
 
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../modals/DarkThemeProvider.dart';
@@ -7,13 +8,17 @@ import '../styles/styles.dart';
 class mainController extends GetxController{
 
   DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+  var userString = "".obs;
 
   @override
   void onInit(){
     super.onInit();
   }
 
-
+//GLOBAL FUNCTIONS
+  hideKeyboard(context){
+    FocusScope.of(context).requestFocus(FocusNode());
+  }
   void getCurrentAppTheme() async {
     themeChangeProvider.darkTheme =
     await themeChangeProvider.darkThemePreference.getTheme();
