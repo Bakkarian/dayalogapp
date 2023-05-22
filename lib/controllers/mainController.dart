669f2ea3b1dd9f7033ee91265d870ccb926b9f91
Ivan Yiga <ivanatresyn@gmail.com>
2023-05-22 12:01:25 +0300

    Moved API key to main controller

--- lib/controllers/mainController.dart
+++ lib/controllers/mainController.dart
@@ -1,4 +1,6 @@
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,6 +8,14 @@ import '../modals/DarkThemeProvider.dart';
 import '../styles/styles.dart';
 
 class mainController extends GetxController{
+
+  var apiKey = "AIzaSyDcWnFHx3aRBy_3oEeDMydOLGECc0MNYjk";
+
+  //Generate random string
+  final _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+  final Random _rnd = Random();
+  String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
+      length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
 
   DarkThemeProvider themeChangeProvider = DarkThemeProvider();
   var userString = "".obs;
